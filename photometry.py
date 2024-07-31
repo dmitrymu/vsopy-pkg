2e90c394a085820c052bd5e2d5c39d1ebb794bf6
dmitrymu <d.v.mukhortov@gmail.com>
2024-07-31 14:55:14 -0700

    Migratie data accessor and  bulk photometry and (#1)
    * Extend readme
    
    * Add initialization script
    
    * Add some layout classes
    
    * Add AAVSO API and parsers
    
    * Refactor and add unit tests
    
    * Add chart parser
    
    * Improve chart test case
    
    * Migrate essential StarData
    
    * cleanup and documenting
    
    * migrate calibration matcher
    
    * Migrate bulk photometry
    
    * migrate photometry aggregation

---
+++ photometry.py
@@ -0,0 +1,38 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(
+    os.path.dirname(__file__), '..')))
+
+import sys
+import argparse
+from vso import phot
+from pathlib import Path
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-O', '--object', type=str, required=True, help='Object to be worked')
+    parser.add_argument('-t', '--tag', type=str, required=True, help='Tag (date)')
+    parser.add_argument('-c', '--common_dir', type=str, required=True, help='File tree root')
+    parser.add_argument('-i', '--image_dir', type=str, default=None, help='Image directory')
+
+    return parser.parse_args()
+
+def main():
+    args = parse_args()
+
+    object_dir = Path(args.tag) / args.object #'20240725/SA38'
+
+    session_dir = args.common_dir /  Path('session') / object_dir
+    calibr_dir =  args.common_dir / Path('img') / 'calibr'
+
+    source_root = args.common_dir / Path('img') / object_dir / 'Light'
+
+    p = phot.BulkPhotometry(session_dir, calibr_dir)
+    p.process(source_root)
+
+    return 0
+
+# Example: python3 photometry.py -O RR_Lyr -t 20230704 -c /srv/public
+
+if __name__ == '__main__':
+    sys.exit(main())
